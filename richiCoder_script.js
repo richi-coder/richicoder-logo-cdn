deb2d25c6ae2d51b3ca348d0cd463fecb660fd0b
richi-coder <richicoder@gmail.com>
2023-05-09 18:24:21 -0400

    before merging

--- richiCoder_script.js
+++ richiCoder_script.js
@@ -1,7 +1,11 @@
+// Create element
 const root = document.createElement('div');
 root.innerHTML = 'richiCoder';
 root.style.position = 'fixed';
 root.style.bottom = 0;
 root.style.left = '50%';
 root.style.transform = 'translateX(-50%)';
-document.body.appendChild(root)
+// Inject html to page
+setTimeout(() => {
+    document.body.appendChild(root)
+}, 200);
