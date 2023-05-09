34a116538380d841a78522ca8ad0ca678a1416b3
richi-coder <richicoder@gmail.com>
2023-05-09 18:02:21 -0400

    commit at gh-pages branch

--- richiCoder_script.js
+++ richiCoder_script.js
@@ -4,4 +4,4 @@ root.style.position = 'fixed';
 root.style.bottom = 0;
 root.style.left = '50%';
 root.style.transform = 'translateX(-50%)';
-document.body.appendChild(root)
+document.body.appendChild(root);
