ae6156cc33b04e8a90331c215c3746b9e4d5b1e0
richi-coder <richicoder@gmail.com>
2023-05-09 19:27:48 -0400

    better performance

--- richiCoder_script.js
+++ richiCoder_script.js
@@ -3,8 +3,14 @@ link.href = 'https://fonts.googleapis.com/css2?family=Orbitron:wght@500;600&disp
 link.rel = 'stylesheet'
 document.head.appendChild(link)
 link.addEventListener('load', () => {
-        console.log('loaded');
-        // Create element
+    createRoot()
+})
+
+
+
+function createRoot() {
+
+    // Create element
         const root = document.createElement('a');
         root.href = 'https://richicoder.com';
         root.target = '_blank';
@@ -12,15 +18,19 @@ link.addEventListener('load', () => {
         root.style.textDecoration = 'none'
         root.innerHTML = 'richiCoder';
         root.style.position = 'fixed';
-        root.style.bottom = 0;
+        root.style.bottom = '1vh';
         root.style.left = '50%';
         root.style.transform = 'translateX(-50%)';
-        root.style.fontFamily = 'Orbitron, arial';
-        root.style.transition = 'all 0.2s ease-out';
+        root.style.fontFamily = 'Orbitron, monospace';
+        root.style.transition = 'transform 0.2s ease-out, opacity 1s ease-in-out';
         root.style.cursor = 'pointer';
-        
+        root.style.opacity = 0
+
         setTimeout(() => {
             document.body.appendChild(root);
+            setTimeout(() => {
+                root.style.opacity = 1
+            }, 500);
             root.addEventListener('mouseover', () => {
                 root.style.transform = 'translateX(-50%) scale(1.2) translateY(-20%)'
             })
@@ -28,6 +38,8 @@ link.addEventListener('load', () => {
                 root.style.transform = 'translateX(-50%)'
             })
         }, 1000);
-})
+
+}
+
 
 
