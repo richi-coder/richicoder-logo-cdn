8c0866dd95d8492278b2572c02cf0f36af904332
richi-coder <richicoder@gmail.com>
2023-05-09 17:49:14 -0400

    first commit from CDN

---
+++ richiCoder_script.js
@@ -0,0 +1,7 @@
+const root = document.createElement('div');
+root.innerHTML = 'richiCoder';
+root.style.position = 'fixed';
+root.style.bottom = 0;
+root.style.left = '50%';
+root.style.transform = 'translateX(-50%)';
+document.body.appendChild(root)
