25c6c7d9e6e3caa495f441fe337fce35a26aeffc
richi-coder <richicoder@gmail.com>
2023-05-09 20:25:45 -0400

    final style

--- richiCoder_script.js
+++ richiCoder_script.js
@@ -10,15 +10,17 @@ function createRoot() {
         root.target = '_blank';
         root.style.color = 'black'
         root.style.textDecoration = 'none'
-        root.innerHTML = 'richiCoder';
+        root.innerHTML = '<div>richi<span id="richi-coder-span-logo-injected">Coder</span></div>';
         root.style.position = 'fixed';
         root.style.bottom = '1vh';
+        
         root.style.left = '50%';
         root.style.transform = 'translateX(-50%)';
         root.style.fontFamily = 'Orbitron, monospace';
         root.style.transition = 'transform 0.2s ease-out, opacity 1s ease-in-out';
         root.style.cursor = 'pointer';
-        root.style.opacity = 0
+        root.style.opacity = 0;
+        
         setTimeout(() => {
             document.body.appendChild(root);
             setTimeout(() => {
@@ -30,5 +32,32 @@ function createRoot() {
             root.addEventListener('mouseleave', () => {
                 root.style.transform = 'translateX(-50%)'
             })
+            // Adding style to CODER
+            const span = document.querySelector('#richi-coder-span-logo-injected');
+            span.style.backgroundImage = 'linear-gradient(45deg, rgb(124,58,237), #00407c 30%, white 60%'
+            span.style.webkitTextFillColor = 'transparent'
+            span.style.webkitBackgroundClip = 'text'
+            span.style.backgroundSize = '400%'
+            span.style.backgroundPosition = 0
         }, 1000);
 }
+
+
+/* 
+:root {
+    --accent: 124, 58, 237;
+    --accent-gradient: linear-gradient(
+      45deg,
+      rgb(var(--accent)),
+      #00407c 30%,
+      white 60%
+    );
+  }
+.text-gradient {
+    background-image: var(--accent-gradient);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-size: 400%;
+    background-position: 0%;
+}
+*/
